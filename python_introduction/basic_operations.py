ebf8399b81d9cf59901b45cc010971c5e4d6b7aa
Mastewal <mastewalgshiferaw@gmail.com>
2025-05-24 01:36:23 +0300

    update on the exercise code again

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,7 +1,7 @@
 number1 = 10
 number2 = 5
-sum=nuumber1+number2
-difference = nuumber2-number1
+sum=number1+number2
+difference = number1-number2
 multiplication = number1*number2
 print("Addition of " + str(number1) + " and " + str(number2) +" is " +str(sum))
 print("Subtraction of " + str(number1) + " and " + str(number2) +" is " + str(difference))
