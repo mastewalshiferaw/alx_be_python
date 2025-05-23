11e60c51aaa7d19c231cfd297b476d519ab7aa0a
Mastewal <mastewalgshiferaw@gmail.com>
2025-05-24 00:05:51 +0300

    committed the first question

---
+++ basic_operations.py
@@ -0,0 +1,9 @@
+int number1 = 10
+int number2 = 5
+int sum=nuumber1+number2
+int difference = nuumber2-number1
+int multiplication = number1*number2
+print("Addition of " + number1 + "and" + number2 +"is" +sum)
+print("Subtraction of " + number1 + "and" + number2 +"is" + difference)
+print("Multiplication of "+number1 + "and" + number2 +"is" + multiplication)
+ 
