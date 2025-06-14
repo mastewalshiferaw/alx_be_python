14ca675a9a4dc9ccf862722281282f00bb7e4dc8
Mastewal <mastewalgshiferaw@gmail.com>
2025-06-15 02:18:14 +0300

    committed task 1

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -6,9 +6,9 @@ def safe_divide(numerator, denominator):
       return "Error: Please enter numeric values only."
 
   except ZeroDivisionError:
-    if float(denominator) == 0:
+    
       return "Error: Cannot divide by zero."
-    else:
+  else:
       return f"The result of the division is {result:.1f}"
   
       
