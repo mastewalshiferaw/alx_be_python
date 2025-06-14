782c3b065517266fc82b7526a320aef56efa2ef6
Mastewal <mastewalgshiferaw@gmail.com>
2025-06-14 21:51:03 +0300

    committed task 0

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,9 +1,12 @@
 class BankAccount:
-  def __init__ (self, initial_balance=0):
-    self.account_balance = float(initial_balance)
+  def __init__ (self, account_balance=0):
+    self.account_balance = account_balance
     
-   def deposit(self, amount):
-    self.account_balance = self.account_balance + amount
+  def deposit(self, amount):
+    if amount > 0:
+      self.account_balance += amount
+      print(f"Deposited: ${amount}")
+
   def withdraw(self, amount):
     if amount > self.account_balance:
       return False
@@ -15,3 +18,4 @@ class BankAccount:
     
   def display_balance(self):
     print (f"Current Balance: ${self.account_balance:.2f}")
+
