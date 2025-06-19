de4d8435b35e54860ad86fb930905c611e66381e
Mastewal <mastewalgshiferaw@gmail.com>
2025-06-20 00:30:22 +0300

    committed project 1

---
+++ oop/library_system.py
@@ -0,0 +1,13 @@
+class Book:
+  def __init__(self, title: str, author: str):
+    self.title = title
+    self.author = author
+class EBook(Book):
+  def __init__(self, title, author, file_size: int):
+    super().__init__(title, author)   
+    self.file_size = file_size
+class PrintBook(Book):
+  def __init__(self, title, author, page_count: int):
+    super().__init__(title, author)
+    self.page_count = page_count
+Library = 
