48060177ef60bd6bc635c78590914de3e29099fa
Mastewal <mastewalgshiferaw@gmail.com>
2025-06-21 02:05:09 +0300

    committed task 3

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -2,7 +2,7 @@ class Calculator:
   claculation_type = "Arithmetic Operations"
   @staticmethod
   def add(a,b):
-    return f"The sum is: {a + b}"
+    return a + b
   @classmethod
   def multiply(cls, a, b):
     print(f"Calculattion type: {cls.calculation_type}")
